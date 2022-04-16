8cd3dd5757a7049768f4c0e2273108671cdc7720
EricaSugui <ericasugui@gmail.com>
2022-04-16 17:53:15 -0300

    adiciona seeds

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,32 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de site',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-04-15 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 6000,
+          createdAt: new Date('2022-04-15 10:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
-    
+
     this.get('/transactions', () => {
       return this.schema.all('transaction')
     })
